79ca9727c36dfa1495a50fe20151d10a7c2517a7
springlordpants <springlordpants@gmail.com>
2020-09-11 11:23:19 -0700

    Project set up

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          i am learning react
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
