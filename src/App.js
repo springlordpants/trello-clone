543c96a9ab9213c80eacbad9735a69633f47cda9
springlordpants <springlordpants@gmail.com>
2020-09-11 13:32:07 -0700

    States and JSX using sample data

--- src/App.js
+++ src/App.js
@@ -2,27 +2,24 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Board from './components/Board';
+import data from './sampleData';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Board />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    boards:[]
+  }
+  updateState = () => {
+    this.setState({boards: data.boards})
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.updateState}></button>
+        {console.table(this.state.boards)}
+        <p>{this.state.boardTitle}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
