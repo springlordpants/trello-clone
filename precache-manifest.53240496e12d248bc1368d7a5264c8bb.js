9189dcece19beeab8cf3d635a25fc0228fa7df4a
springlordpants <springlordpants@gmail.com>
2020-09-28 14:38:57 -0700

    Updates

---
+++ precache-manifest.53240496e12d248bc1368d7a5264c8bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8460d202da0eca1544a360f07c451d7",
+    "url": "/trello-clone/index.html"
+  },
+  {
+    "revision": "7fed0da965366e6ed712",
+    "url": "/trello-clone/static/css/main.b954e2ff.chunk.css"
+  },
+  {
+    "revision": "56834578ef1c05869e51",
+    "url": "/trello-clone/static/js/2.cc29ab4f.chunk.js"
+  },
+  {
+    "revision": "0d254d6a32983b123964d0360c7d6722",
+    "url": "/trello-clone/static/js/2.cc29ab4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fed0da965366e6ed712",
+    "url": "/trello-clone/static/js/main.6b8ba5c0.chunk.js"
+  },
+  {
+    "revision": "771355ea2405392386a6",
+    "url": "/trello-clone/static/js/runtime-main.7453dcbd.js"
+  }
+]);
